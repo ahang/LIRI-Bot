766a0fe3d1b9b5c5456606f58bd557873d26cda6
Andy Hang <hang.andy.t@gmail.com>
2017-04-03 10:59:19 -0500

    adding myTweet function. WIP

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('Loaded ze twitter keys');
+
+exports.twitterKeys = {
+  consumer_key: 'PORlKJDfnCFuYHFlhTy5m3LPF',
+  consumer_secret: 'rOWW03r4uluX1JgLeq3dan4etiYWE2YfK3wIWWxuegtJvo0n8G',
+  access_token_key: '847625093526179841-CIvPZy8x7ID7xpCgeADid5otBhvO5P1',
+  access_token_secret: '9MX9sRlai71GZw8zOBD8hlowZra8K4QK68YFpb0qfWxhj',
+}
---
+++ liri.js
@@ -0,0 +1,29 @@
+//---------TWITTER-------------
+var twitter = require("twitter");
+var keys = require("./key.js");
+var twitKeys = new twitter(keys.twitterKeys);
+
+//console.log(twitKeys);
+//-----------------------------
+
+var myTweets = function() {
+    var params = {
+        screen_name: "ahang_1",
+        count: 20
+    }
+
+    twitKeys.get("statuses/user_timeline", params, function(err, data) {
+        if (!err) {
+            console.log("About to test loop");
+            for (var i = 0; i < data.length; i++) {
+                console.log("Testing Loop");
+                var tweets = data[i].text;
+                console.log(tweets);
+            }
+        } else {
+            console.log("There was an err of " + err);
+        }
+    });
+};
+
+myTweets;
