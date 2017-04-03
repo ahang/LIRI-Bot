c03547fd1bbfad1a6a47c0f83127b00870ae2289
Andy Hang <hang.andy.t@gmail.com>
2017-04-03 15:47:34 -0500

    movie function

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 //---------DEPENDENCIES-------------
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //insecure workaround for firewall. temporary
 
-var twitter = require("twitter");
+var twitter = require("twitter"); //grabbing twitter npm
 var keys = require("./key.js");
 var twitKeys = new twitter(keys.twitterKeys);
 var spotify = require("spotify");
@@ -60,18 +60,18 @@ function myTweets() {
 };
 
 function findTrack(userInput) {
-    console.log("Test");
+    //console.log("Test");
     var trackName = userInput || "The Sign Ace of Base";
-    console.log(trackName);
+    //console.log(trackName);
 
     spotify.search({ type: "track", query: trackName}, function(err, data) {
-        console.log("Beginning Query");
+        //console.log("Beginning Query");
         if (err) {
              console.log('Error occurred: ' + err);
              return;
         } else {
             var track = data.tracks.items[0];
-            console.log("No errors beginning data retrieval");
+            //console.log("No errors beginning data retrieval");
             console.log("The Artist(s) is " + track.artists[0].name);
             console.log("The name of the song is " + "'" + track.name + "'");
             console.log("Link: " + track.preview_url);
@@ -79,4 +79,31 @@ function findTrack(userInput) {
         }
     });
 }
+
+function movie(userInput) {
+    console.log("Begin movie function");
+
+    var movieName = userInput || "Mr. Nobody";
+    console.log(movieName);
+
+    request("http://www.omdbapi.com/?t=" + movieName, function(err, response, body) {
+        if (err) {
+            console.log("Error occured: " + err);
+        } else {
+            var movieInfo = JSON.parse(body);
+            //console.log(movieInfo);
+            //console.log(JSON.parse(body));
+            console.log("---------------Movie Information-------------")
+            console.log("The title of the movie is " + movieInfo.Title);
+            console.log("The movie was released " + movieInfo.Year);
+            console.log("The IMDB Rating for the movie is " + movieInfo.imdbRating);
+            console.log("The movie was produced in " + movieInfo.Country);
+            console.log("The languages in the movie are " + movieInfo.Language);
+            console.log("The main actors/actresses are " + movieInfo.Actors);
+            console.log("On Rotten Tomato, they gave it a rating of " + movieInfo.Ratings[1].Value);
+            console.log("----------------------------")
+        }
+    });
+}
+
 //-----------------------------
