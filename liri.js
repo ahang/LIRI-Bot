77f6a08d8a1f257cb97990fe21d0c933e2e2d130
Andy Hang <hang.andy.t@gmail.com>
2017-04-04 12:24:44 -0500

    testing new function for append

--- liri.js
+++ liri.js
@@ -9,7 +9,6 @@ var twitKeys = new twitter(keys.twitterKeys);
 var spotify = require("spotify");
 var request = require("request");
 var fs = require("fs");
-var input;
 
 //console.log(twitKeys);
 //-----------------------------
@@ -18,7 +17,7 @@ var input;
 var action = process.argv[2];
 var command = process.argv[3];
 
-values("my-tweet");
+values("movie-this");
 
 function values(action, command) {
     switch (action) {
@@ -39,29 +38,40 @@ function values(action, command) {
         break;
     }
 }
+//appending the data from the logs
+function logs(data) {
+    fs.appendFile("logs.txt", data, function(err, data) {
+        if (err) throw err;
+    })
+}
+//console.logging the data and calling the logs function for each case
+function debug(str) {
+    console.log(str);
+    logs(str + "\n");
+
+}
 
 function myTweets() {
-
+    //pass the screen_name and count parameter into .get for twitter
     var params = {
         screen_name: "ahang_",
         count: 20
     }
 
     twitKeys.get("statuses/user_timeline", params, function(err, data) {
-        if (!err) {
+        if (!err) { //if no error
             //console.log("About to test loop");
             for (var i = 0; i < data.length; i++) {
                 
                 //console.log("Testing Loop" + [i]);
+                var time = data[i].created_at;
                 var user = params.screen_name;
                 var tweets = data[i].text;
-                var time = data[i].created_at;
-                input = ("--------" + time + "--------------------" + "\n" +
-                "                 @" + user + " said" + "\n" +
-                "         " + tweets + "\n" +
-                "----------------------------------------------------------");
-                console.log(input);
-                logs(input);
+                debug("------------------------------------------------------");
+                debug(time);
+                debug("@" + user + " said");
+                debug(tweets);
+                debug("------------------------------------------------------");
             }
             //console.log(tweets);
         } else {
@@ -84,18 +94,17 @@ function findTrack(userInput) {
             var track = data.tracks.items[0];
             //console.log(track);
             //console.log("No errors beginning data retrieval");
-            input = ()
-            console.log("The Artist(s) is " + track.artists[0].name);
-            console.log("The name of the song is " + "'" + track.name + "'");
-            console.log("Link: " + track.preview_url);
-            console.log("The album is called " + track.album.name + " and this song is track number " + track.track_number);
+            debug("The Artist(s) is " + track.artists[0].name);
+            debug("The name of the song is " + "'" + track.name + "'");
+            debug("Link: " + track.preview_url);
+            debug("The album is called " + track.album.name + " and this song is track number " + track.track_number);
         }
     });
 }
 
 function movie(userInput) {
     //console.log("Begin movie function");
-
+    console.log(userInput);
     var movieName = userInput.split(" ").join("+") || "Mr.+Nobody";
     //console.log(movieName);
 
@@ -108,18 +117,18 @@ function movie(userInput) {
         } else {
             //console.log(movieInfo);
             //console.log(JSON.parse(body));
-            console.log("---------------Movie Information-------------")
-            console.log("The title of the movie is " + movieInfo.Title);
-            console.log("The movie was released " + movieInfo.Year);
-            console.log("The IMDB Rating for the movie is " + movieInfo.imdbRating);
-            console.log("The movie was produced in " + movieInfo.Country);
-            console.log("The languages in the movie are " + movieInfo.Language);
-            console.log("The main actors/actresses are " + movieInfo.Actors);
-            console.log("On Rotten Tomato, they gave it a rating of " + movieInfo.Ratings[1].Value);
+            debug("---------------Movie Information-------------")
+            debug("The title of the movie is " + movieInfo.Title);
+            debug("The movie was released " + movieInfo.Year);
+            debug("The IMDB Rating for the movie is " + movieInfo.imdbRating);
+            debug("The movie was produced in " + movieInfo.Country);
+            debug("The languages in the movie are " + movieInfo.Language);
+            debug("The main actors/actresses are " + movieInfo.Actors);
+            debug("On Rotten Tomato, they gave it a rating of " + movieInfo.Ratings[1].Value);
             var movieChange = movieInfo.Title.split(" ").join("_");
             var lowerMovie = movieChange.toLowerCase();
-            console.log("Check out the Rotten Tomato review here https://www.rottentomatoes.com/m/" + lowerMovie);
-            console.log("----------------------------");
+            debug("Check out the Rotten Tomato review here https://www.rottentomatoes.com/m/" + lowerMovie);
+            debug("----------------------------");
         }
     });
 }
@@ -136,10 +145,6 @@ function doIt() {
         values(dataArr[0].trim(),dataArr[1].trim()); //feeding the dataArr into the values function
     });
 }
-
-var logs = function(data) {
-    fs.appendFile("logs.txt", data, function(err, data) {
-        if (err) throw err;
-    })
-}
 //-----------------------------
+
+
